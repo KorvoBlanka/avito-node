c8f4c1aca9243879621855b269fa25c429bcb11e
KorvoBlanka <korvo.blanka@gmail.com>
2016-06-27 08:24:16 +0300

    некоторые параметры вынесены в конфиг, удалено лишнее

--- Util/Config.pm
+++ Util/Config.pm
@@ -1,19 +1,12 @@
-package Import::Util::Config;
+package Util::Config;
 
-use Cwd qw/abs_path/;
 use Mojo::Asset::File;
 
 sub get_config {
-    # get path to Config.pm, then build a path to app.conf
-    my $module = __PACKAGE__;
-    $module =~s/::/\//g;
-    my $path = $INC{$module . '.pm'};
-    $path =~ s{^(.*/)[^/]*$}{$1};
-    $path = abs_path($path . '/../../../app.conf');
-    
-    my $file = Mojo::Asset::File->new(path => $path);
-    my $config = eval $file->slurp;
-    return $config;
+  my $path = 'config';
+  my $file = Mojo::Asset::File->new(path => $path);
+  my $config = eval $file->slurp;
+  return $config;
 }
 
 1;
